8b8889b65b3c3e5e739c4ed64c65f68c431c51f5
Aline <aline.onishi@gmail.com>
2017-01-05 16:54:50 -0200

    Add all python scripts

---
+++ Livro/list_file.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+import os
+
+for meta_file in os.listdir('data/meta-data'):
+    print(meta_file)
+    
---
+++ break_time.py
@@ -0,0 +1,24 @@
+Last login: Wed Aug  3 10:01:03 on ttys000
+sap2aclaonis012:~ aonishi$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
